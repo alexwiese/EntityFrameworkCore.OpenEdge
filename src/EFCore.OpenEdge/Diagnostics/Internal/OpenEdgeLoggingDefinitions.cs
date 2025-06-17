199c9c47d8cc984d3905d4cad7f4224207a40422
Ernests <rudzitisernests1@gmail.com>
2025-06-17 18:29:49 +0200

    Successful initial .NET Core 3.1 migration

---
+++ src/EFCore.OpenEdge/Diagnostics/Internal/OpenEdgeLoggingDefinitions.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EntityFrameworkCore.OpenEdge.Diagnostics.Internal
+{
+    public class OpenEdgeLoggingDefinitions : RelationalLoggingDefinitions
+    {
+    }
+}
