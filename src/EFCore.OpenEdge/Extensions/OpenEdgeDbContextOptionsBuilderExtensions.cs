75f9eb35a10d6eaae4e306e98a543d0da38110e9
Ernests <rudzitisernests1@gmail.com>
2025-07-21 13:20:35 +0200

    Database schema configuration param

--- src/EFCore.OpenEdge/Extensions/OpenEdgeDbContextOptionsBuilderExtensions.cs
+++ src/EFCore.OpenEdge/Extensions/OpenEdgeDbContextOptionsBuilderExtensions.cs
@@ -9,9 +9,33 @@ namespace Microsoft.EntityFrameworkCore
 {
     public static class OpenEdgeDbContextOptionsBuilderExtensions
     {
+        /// <summary>
+        /// Configures the context to connect to an OpenEdge database.
+        /// </summary>
+        /// <param name="optionsBuilder">The builder being used to configure the context.</param>
+        /// <param name="connectionString">The connection string of the database to connect to.</param>
+        /// <param name="optionsAction">An optional action to allow additional configuration.</param>
+        /// <returns>The options builder so that further configuration can be chained.</returns>
         public static DbContextOptionsBuilder UseOpenEdge(
             this DbContextOptionsBuilder optionsBuilder,
             string connectionString,
+            Action<DbContextOptionsBuilder> optionsAction = null)
+        {
+            return UseOpenEdge(optionsBuilder, connectionString, defaultSchema: null, optionsAction);
+        }
+
+        /// <summary>
+        /// Configures the context to connect to an OpenEdge database with a specific default schema.
+        /// </summary>
+        /// <param name="optionsBuilder">The builder being used to configure the context.</param>
+        /// <param name="connectionString">The connection string of the database to connect to.</param>
+        /// <param name="defaultSchema">The default schema to use for tables when not explicitly specified. Defaults to "pub" if null.</param>
+        /// <param name="optionsAction">An optional action to allow additional configuration.</param>
+        /// <returns>The options builder so that further configuration can be chained.</returns>
+        public static DbContextOptionsBuilder UseOpenEdge(
+            this DbContextOptionsBuilder optionsBuilder,
+            string connectionString,
+            string defaultSchema,
             Action<DbContextOptionsBuilder> optionsAction = null)
         {
             /*
@@ -27,6 +51,12 @@ namespace Microsoft.EntityFrameworkCore
              *      options.UseOpenEdge("connection"));  // Automatic registration
              */
             var extension = GetOrCreateExtension(optionsBuilder).WithConnectionString(connectionString);
+            
+            if (defaultSchema != null)
+            {
+                extension = ((OpenEdgeOptionsExtension) extension).WithDefaultSchema(defaultSchema);
+            }
+            
             ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
 
             optionsAction?.Invoke(optionsBuilder);
@@ -34,13 +64,38 @@ namespace Microsoft.EntityFrameworkCore
             return optionsBuilder;
         }
 
+        /// <summary>
+        /// Configures the context to connect to an OpenEdge database.
+        /// </summary>
+        /// <typeparam name="TContext">The type of context to be configured.</typeparam>
+        /// <param name="optionsBuilder">The builder being used to configure the context.</param>
+        /// <param name="connectionString">The connection string of the database to connect to.</param>
+        /// <param name="optionsAction">An optional action to allow additional configuration.</param>
+        /// <returns>The options builder so that further configuration can be chained.</returns>
         public static DbContextOptionsBuilder<TContext> UseOpenEdge<TContext>(
             this DbContextOptionsBuilder<TContext> optionsBuilder,
             string connectionString,
             Action<DbContextOptionsBuilder> optionsAction = null)
             where TContext : DbContext
+            => UseOpenEdge(optionsBuilder, connectionString, defaultSchema: null, optionsAction);
+
+        /// <summary>
+        /// Configures the context to connect to an OpenEdge database with a specific default schema.
+        /// </summary>
+        /// <typeparam name="TContext">The type of context to be configured.</typeparam>
+        /// <param name="optionsBuilder">The builder being used to configure the context.</param>
+        /// <param name="connectionString">The connection string of the database to connect to.</param>
+        /// <param name="defaultSchema">The default schema to use for tables when not explicitly specified. Defaults to "pub" if null.</param>
+        /// <param name="optionsAction">An optional action to allow additional configuration.</param>
+        /// <returns>The options builder so that further configuration can be chained.</returns>
+        public static DbContextOptionsBuilder<TContext> UseOpenEdge<TContext>(
+            this DbContextOptionsBuilder<TContext> optionsBuilder,
+            string connectionString,
+            string defaultSchema,
+            Action<DbContextOptionsBuilder> optionsAction = null)
+            where TContext : DbContext
             => (DbContextOptionsBuilder<TContext>)UseOpenEdge(
-                (DbContextOptionsBuilder)optionsBuilder, connectionString, optionsAction);
+                (DbContextOptionsBuilder)optionsBuilder, connectionString, defaultSchema, optionsAction);
 
         private static OpenEdgeOptionsExtension GetOrCreateExtension(DbContextOptionsBuilder optionsBuilder) 
             => optionsBuilder.Options.FindExtension<OpenEdgeOptionsExtension>() ?? new OpenEdgeOptionsExtension();
--- src/EFCore.OpenEdge/Infrastructure/Internal/OpenEdgeOptionsExtension.cs
+++ src/EFCore.OpenEdge/Infrastructure/Internal/OpenEdgeOptionsExtension.cs
@@ -15,12 +15,40 @@ namespace EntityFrameworkCore.OpenEdge.Infrastructure.Internal
     public class OpenEdgeOptionsExtension : RelationalOptionsExtension
     {
         private DbContextOptionsExtensionInfo _info;
+        private string _defaultSchema;
+        
+        public OpenEdgeOptionsExtension()
+        {
+        }
+        
+        protected OpenEdgeOptionsExtension(OpenEdgeOptionsExtension copyFrom) : base(copyFrom)
+        {
+            _defaultSchema = copyFrom._defaultSchema;
+        }
+        
+        /// <summary>
+        /// The default schema to use for tables when not explicitly specified.
+        /// Defaults to "pub" if not set.
+        /// </summary>
+        public virtual string DefaultSchema => _defaultSchema ?? "pub";
         
         public override DbContextOptionsExtensionInfo Info 
             => _info ?? (_info = new OpenExtensionInfo(this));
 
         protected override RelationalOptionsExtension Clone() 
-            => new OpenEdgeOptionsExtension();
+            => new OpenEdgeOptionsExtension(this);
+        
+        /// <summary>
+        /// Returns a new instance with the specified default schema.
+        /// </summary>
+        /// <param name="defaultSchema">The default schema name to use.</param>
+        /// <returns>A new OpenEdgeOptionsExtension instance with the specified default schema.</returns>
+        public virtual OpenEdgeOptionsExtension WithDefaultSchema(string defaultSchema)
+        {
+            var clone = new OpenEdgeOptionsExtension(this);
+            clone._defaultSchema = defaultSchema;
+            return clone;
+        }
 
         public override void ApplyServices(IServiceCollection services)
         {
@@ -41,7 +69,12 @@ namespace EntityFrameworkCore.OpenEdge.Infrastructure.Internal
             public override string LogFragment => "using OpenEdge";
 
             public override void PopulateDebugInfo(IDictionary<string, string> debugInfo)
-                => debugInfo["OpenEdge"] = "1";
+            {
+                debugInfo["OpenEdge"] = "1";
+                
+                var openEdgeExtension = (OpenEdgeOptionsExtension)Extension;
+                debugInfo["OpenEdge:DefaultSchema"] = openEdgeExtension.DefaultSchema;
+            }
             
         }
     }
